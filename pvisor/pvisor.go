033196d2b8f22c66c2161ca3efa36bcca51350af
Reza <reza@ies-italia.it>
2019-08-18 18:30:17 +0430

    update

--- pvisor/pvisor.go
+++ pvisor/pvisor.go
@@ -6,43 +6,42 @@ import (
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"net/http"
-	"packet-visor/helper"
 )
 
-
 var seq int64
-var  DebugURL string
-func DebugPacket(p gopacket.Packet,mark string)  {
+var DebugURL string
 
-	wrapper := helper.Packet{}
+func DebugPacket(p gopacket.Packet, mark string) {
+
+	wrapper := Packet{}
 
 	b := p.Data()
 	tcpLayer := p.Layer(layers.LayerTypeTCP)
 	udpLayer := p.Layer(layers.LayerTypeUDP)
 
-	wrapper.SrcIP = helper.ParseSrcIP(b)
-	wrapper.DstIP = helper.ParseDstIP(b)
+	wrapper.SrcIP = ParseSrcIP(b)
+	wrapper.DstIP = ParseDstIP(b)
 	if tcpLayer != nil || udpLayer != nil {
-		wrapper.SrcPort = helper.ParseSrcPort(b)
-		wrapper.DstPort = helper.ParseDstPort(b)
+		wrapper.SrcPort = ParseSrcPort(b)
+		wrapper.DstPort = ParseDstPort(b)
 	}
 
 	wrapper.Time = p.Metadata().Timestamp.Unix()
 	seq++
 	wrapper.Sequence = seq
-	if tcpLayer != nil{
+	if tcpLayer != nil {
 		wrapper.Type = "TCP"
 		wrapper.LayerData = tcpLayer.LayerPayload()
-	}else if udpLayer != nil{
+	} else if udpLayer != nil {
 		wrapper.Type = "UDP"
 		wrapper.LayerData = udpLayer.LayerPayload()
-	}else{
+	} else {
 		wrapper.Type = "IP"
 	}
 	wrapper.Mark = mark
 
-	b,err := json.Marshal(wrapper)
-	if err == nil{
+	b, err := json.Marshal(wrapper)
+	if err == nil {
 
 		req, err := http.NewRequest("POST", DebugURL, bytes.NewBuffer(b))
 		req.Header.Set("Content-Type", "application/json")
