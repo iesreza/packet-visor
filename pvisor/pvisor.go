ed5b8a930f5d11e4bcc327fc8ee2d9df811078d3
Reza <reza@ies-italia.it>
2019-08-18 19:06:48 +0430

    update

--- pvisor/pvisor.go
+++ pvisor/pvisor.go
@@ -13,46 +13,47 @@ var seq int64
 var DebugURL string
 
 func DebugPacket(p gopacket.Packet, mark string) {
+	go func() {
+		wrapper := Packet{}
 
-	wrapper := Packet{}
+		b := p.Data()
+		tcpLayer := p.Layer(layers.LayerTypeTCP)
+		udpLayer := p.Layer(layers.LayerTypeUDP)
 
-	b := p.Data()
-	tcpLayer := p.Layer(layers.LayerTypeTCP)
-	udpLayer := p.Layer(layers.LayerTypeUDP)
-
-	wrapper.SrcIP = ParseSrcIP(b)
-	wrapper.DstIP = ParseDstIP(b)
-	if tcpLayer != nil || udpLayer != nil {
-		wrapper.SrcPort = ParseSrcPort(b)
-		wrapper.DstPort = ParseDstPort(b)
-	}
-
-	wrapper.Time = p.Metadata().Timestamp.Unix()
-	seq++
-	wrapper.Sequence = seq
-	if tcpLayer != nil {
-		wrapper.Type = "TCP"
-		wrapper.LayerData = tcpLayer.LayerPayload()
-	} else if udpLayer != nil {
-		wrapper.Type = "UDP"
-		wrapper.LayerData = udpLayer.LayerPayload()
-	} else {
-		wrapper.Type = "IP"
-	}
-	wrapper.Mark = mark
-
-	b, err := json.Marshal(wrapper)
-	if err == nil {
-		log.Warning("Send packet to %s", DebugURL)
-		req, err := http.NewRequest("POST", DebugURL, bytes.NewBuffer(b))
-		req.Header.Set("Content-Type", "application/json")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Error(err)
+		wrapper.SrcIP = ParseSrcIP(b)
+		wrapper.DstIP = ParseDstIP(b)
+		if tcpLayer != nil || udpLayer != nil {
+			wrapper.SrcPort = ParseSrcPort(b)
+			wrapper.DstPort = ParseDstPort(b)
 		}
-		defer resp.Body.Close()
 
-	}
+		wrapper.Time = p.Metadata().Timestamp.Unix()
+		seq++
+		wrapper.Sequence = seq
+		if tcpLayer != nil {
+			wrapper.Type = "TCP"
+			wrapper.LayerData = tcpLayer.LayerPayload()
+		} else if udpLayer != nil {
+			wrapper.Type = "UDP"
+			wrapper.LayerData = udpLayer.LayerPayload()
+		} else {
+			wrapper.Type = "IP"
+		}
+		wrapper.Mark = mark
+
+		b, err := json.Marshal(wrapper)
+		if err == nil {
+			log.Warning("Send packet to %s", DebugURL)
+			req, err := http.NewRequest("POST", DebugURL, bytes.NewBuffer(b))
+			req.Header.Set("Content-Type", "application/json")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Error(err)
+			}
+			defer resp.Body.Close()
+
+		}
+	}()
 }
